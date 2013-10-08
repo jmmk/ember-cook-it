e7ee225c3c5fe9227d324f499c01ec7dfffe5e2f
Michael McLellan <jmikem825@gmail.com>
2013-10-07 21:11:21 -0400

    Configure environments and add latest ember and ember-data

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,5 @@ EmberCookIt::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.ember_variant = :development
 end
